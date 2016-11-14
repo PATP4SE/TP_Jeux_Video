4e870652749594bc1264be2f581bccf2daede2e7
PATP4SE <p-a_morin@hotmail.com>
2016-11-14 12:10:24 -0500

    Player

---
+++ CrushSpaceTime/Assets/Scripts/PlayerMovement/PlayerMove.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMove : MonoBehaviour {
+
+    [SerializeField] private float speed;
+    private SpriteRenderer sprite;
+    // Use this for initialization
+    void Start () {
+        sprite = GetComponent<SpriteRenderer>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            transform.Translate(0, speed, 0);
+            sprite.flipY = false;
+        }
+        else if (Input.GetKeyDown(KeyCode.A))
+        {
+            transform.Translate(-speed, 0, 0);
+            sprite.flipX = false;
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            transform.Translate(0, -speed, 0);
+            sprite.flipY = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            transform.Translate(speed, 0, 0);
+            sprite.flipX = true;
+        }
+    }
+}
