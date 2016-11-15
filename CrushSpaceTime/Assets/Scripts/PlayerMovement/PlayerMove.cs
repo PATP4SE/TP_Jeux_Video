90575e60bdf5653b69e91767ad669a97d16c20cc
PATP4SE <p-a_morin@hotmail.com>
2016-11-15 18:38:50 -0500

    Test + rotation

--- CrushSpaceTime/Assets/Scripts/PlayerMovement/PlayerMove.cs
+++ CrushSpaceTime/Assets/Scripts/PlayerMovement/PlayerMove.cs
@@ -3,35 +3,69 @@ using System.Collections;
 
 public class PlayerMove : MonoBehaviour {
 
-    [SerializeField] private float speed;
+    [SerializeField] private float speed = 0;
+    [SerializeField] private float decreasePercentage = 0;
+
     private SpriteRenderer sprite;
+    private Rigidbody2D rb;
+
+
     // Use this for initialization
     void Start () {
         sprite = GetComponent<SpriteRenderer>();
+        rb = GetComponent<Rigidbody2D>();
     }
-	
-	// Update is called once per frame
-	void Update ()
+
+    // Update is called once per frame
+    void Update()
     {
         if (Input.GetKeyDown(KeyCode.W))
         {
+            transform.rotation = new Quaternion(0, 0, 0, 0);
             transform.Translate(0, speed, 0);
-            sprite.flipY = false;
         }
         else if (Input.GetKeyDown(KeyCode.A))
         {
+            transform.rotation = new Quaternion(0, 0, 0, 0);
             transform.Translate(-speed, 0, 0);
-            sprite.flipX = false;
+
+            rotate(0, 0, 90);
         }
         else if (Input.GetKeyDown(KeyCode.S))
         {
+            transform.rotation = new Quaternion(0, 0, 0, 0);
             transform.Translate(0, -speed, 0);
-            sprite.flipY = true;
+
+            rotate(0, 0, 180);
         }
         else if (Input.GetKeyDown(KeyCode.D))
         {
+            transform.rotation = new Quaternion(0, 0, 0, 0);
             transform.Translate(speed, 0, 0);
-            sprite.flipX = true;
+
+            rotate(0, 0, -90);
+        }
+
+        if (!((rb.velocity.x <= 0.0001 && rb.velocity.x >= -0.0001) && (rb.velocity.y <= 0.0001 && rb.velocity.y >= -0.0001)))
+        {
+            print("asdasdasd");
+            DecreaseSpeed();
+            
         }
     }
+
+    /************************************ PRIVATE METHODS ************************************/
+    private void rotate(float x, float y, float z)
+    {
+        Quaternion quat = transform.rotation;
+        quat.eulerAngles = new Vector3(x, y, z);
+        transform.rotation = quat;
+    }
+
+    private void DecreaseSpeed()
+    {
+        rb.velocity = new Vector2(rb.velocity.x * (decreasePercentage / 100), rb.velocity.y * (decreasePercentage / 100));
+    }
+
+
 }
