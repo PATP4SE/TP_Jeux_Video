16fc19326e132bcad2b0a1a3f2d8720e6e27b1d9
WilliamJose-SimardTouzet <will_slipknot93@hotmail.com>
2016-11-16 17:19:12 -0500

    Add asteroids spawn, add sound when pressing movement key.

---
+++ CrushSpaceTime/Assets/Scripts/Mover.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mover : MonoBehaviour {
+
+    [SerializeField] private float maxSpeed = 0;
+    [SerializeField] private float minSpeed = 0;
+    private Rigidbody2D rbody;
+
+    void Start()
+    {
+        float speed = Random.Range(minSpeed, maxSpeed);
+        rbody = GetComponent<Rigidbody2D>();
+        rbody.velocity = new Vector2(0, speed);
+    }
+}
