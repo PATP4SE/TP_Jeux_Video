e2cce786d43852ea8c46ee8c4ed3af132eb37049
WilliamJose-SimardTouzet <will_slipknot93@hotmail.com>
2016-11-17 16:20:35 -0500

    Add background music, begin time manipulation

--- CrushSpaceTime/Assets/Scripts/GameController.cs
+++ CrushSpaceTime/Assets/Scripts/GameController.cs
@@ -9,9 +9,29 @@ public class GameController : MonoBehaviour {
     [SerializeField] private float spawnWait;
     [SerializeField] private float startWait;
     [SerializeField] private float waveWait;
+    [SerializeField] private float timeSpeed;
+    private AudioSource musicBackground;
 
     void Start () {
+        musicBackground = GetComponent<AudioSource>();
         StartCoroutine(spawnWaves());
+    }
+
+    void Update()
+    {
+        if (timeSpeed != 1f)
+        {
+            musicBackground.pitch = Time.timeScale * timeSpeed;
+            GameObject[] asteroids = GameObject.FindGameObjectsWithTag("Asteroid");
+            foreach(GameObject asteroid in asteroids)
+            {
+                Rigidbody2D rbody = asteroid.GetComponent<Rigidbody2D>();
+                Vector2 oldVelocity = rbody.velocity;
+                Debug.Log("oldVelocity" + oldVelocity.y);
+                rbody.velocity = new Vector2(0, oldVelocity.y*timeSpeed);
+                Debug.Log("newVelocity" + rbody.velocity.y);
+            }
+        }
     }
 
     IEnumerator spawnWaves()
