7e59a5b64657ea261a37b7f0a50a21811d6b176d
PATP4SE <p-a_morin@hotmail.com>
2016-12-13 14:46:35 -0500

    Arrangement du HUD

---
+++ CrushSpaceTime/Assets/Scripts/HUD/PlaceDustText.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlaceDustText : MonoBehaviour {
+
+    [SerializeField] private Vector2 startPosition;
+
+	// Use this for initialization
+	void Start ()
+    {
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+}
